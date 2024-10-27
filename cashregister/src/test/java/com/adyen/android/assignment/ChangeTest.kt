5252a6f526eba9effb4b696aca3591fb90e0b726
Omar El Hefny <elhefny@Omars-MacBook-Pro.local>
2024-10-27 21:16:01 +0300

    test: make sure Change class working correctly

--- cashregister/src/test/java/com/adyen/android/assignment/ChangeTest.kt
+++ cashregister/src/test/java/com/adyen/android/assignment/ChangeTest.kt
@@ -5,6 +5,7 @@ import com.adyen.android.assignment.money.Change
 import com.adyen.android.assignment.money.Coin
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotEquals
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class ChangeTest {
@@ -50,5 +51,69 @@ class ChangeTest {
             .add(Bill.ONE_HUNDRED_EURO, 1)
         assertNotEquals(expected, actual)
     }
+
+    @Test
+    fun `test adding single element increases count and updates total`() {
+        val change = Change()
+        change.add(Bill.TEN_EURO, 2)
+
+        assertEquals(2, change.getCount(Bill.TEN_EURO))
+        assertEquals(20_00, change.total) // 2 x 10 Euro
+    }
+
+    @Test
+    fun `test adding multiple elements updates total and counts correctly`() {
+        val change = Change()
+        change.add(Bill.FIFTY_EURO, 1)
+        change.add(Coin.TWENTY_CENT, 5)
+
+        assertEquals(1, change.getCount(Bill.FIFTY_EURO))
+        assertEquals(5, change.getCount(Coin.TWENTY_CENT))
+        assertEquals(50_00 + (5 * 20), change.total) // 50 Euro + 5 x 0.20 Euro
+    }
+
+    @Test
+    fun `test removing elements decreases count and updates total`() {
+        val change = Change()
+        change.add(Coin.TWO_EURO, 3)
+        change.remove(Coin.TWO_EURO, 1)
+
+        assertEquals(2, change.getCount(Coin.TWO_EURO))
+        assertEquals(4_00, change.total)
+    }
+
+    @Test
+    fun `test removing all elements sets count to zero and updates total`() {
+        val change = Change()
+        change.add(Bill.FIVE_HUNDRED_EURO, 1)
+        change.remove(Bill.FIVE_HUNDRED_EURO, 1)
+
+        assertEquals(0, change.getCount(Bill.FIVE_HUNDRED_EURO))
+        assertEquals(0, change.total)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun `test removing more elements than available throws exception`() {
+        val change = Change()
+        change.add(Coin.ONE_CENT, 2)
+        change.remove(Coin.ONE_CENT, 3)
+    }
+
+    @Test
+    fun `elements should be sorted from smallest to largest in Change`() {
+        val change = Change()
+        change.add(Bill.TWENTY_EURO, 1)
+        change.add(Coin.FIVE_CENT, 3)
+        change.add(Bill.FIFTY_EURO, 1)
+        change.add(Coin.TWO_EURO, 2)
+
+        val sortedElements = change.getElements().toList()
+        val isSorted = sortedElements.zipWithNext { a, b ->
+            a.minorValue <= b.minorValue
+        }.all { it }
+
+        assertTrue("Elements in Change should be sorted from smallest to largest",isSorted)
+    }
+
 
 }
