2ea9ea357e4949d8f550164cce0e3be96c66ab4d
Omar El Hefny <elhefny@Omars-MacBook-Pro.local>
2024-11-04 19:32:05 +0200

    Feat: minor UI enhancments in colors

--- app/src/main/java/com/adyen/android/assignment/venues/presentation/ui/FiltersSection.kt
+++ app/src/main/java/com/adyen/android/assignment/venues/presentation/ui/FiltersSection.kt
@@ -3,17 +3,23 @@ package com.adyen.android.assignment.venues.presentation.ui
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.selection.toggleable
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.CheckboxDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.adyen.android.assignment.R
 
 
 @Composable
@@ -23,7 +29,20 @@ fun FiltersSection(
     verifiedChecked: Boolean,
     onVerifiedCheckedChange: (Boolean) -> Unit
 ) {
-    Row() {
+    Row(Modifier.padding(horizontal = 16.dp)) {
+        Icon(
+            imageVector = ImageVector.vectorResource(id = R.drawable.ic_filter_24),
+            contentDescription = "Filter Icon",
+            tint = MaterialTheme.colorScheme.onSurface,
+            modifier = Modifier.size(24.dp)
+        )
+        Text(
+            text = "Filter By:",
+            style = MaterialTheme.typography.bodyMedium,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+
         FilterCheckbox(
             checked = openNowChecked,
             onCheckedChange = onOpenNowCheckedChange,
